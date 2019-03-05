a06fc8fe8eb74fcb2569f335712a65318e5d246d
Braden Bird <bradenbird@tamu.edu>
2019-03-05 17:05:57 -0600

    Added scripts for fire extinguisher, need to work out particle effect and assets

---
+++ Assets/Scripts/FireExtinguisher.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class FireExtinguisher : MonoBehaviour {
+
+    public SteamVR_Action_Boolean m_ActivateAction = null;
+
+    // Use this for initialization
+    void Awake ()
+    {
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        Interactable m_Interactable = GetComponent<Interactable>();
+
+        // Is the fire extinguisher being held?
+        if (m_Interactable.m_ActiveHand)
+        {
+            // Object is being held, we can now determine if we should be
+            // activating the fire extinguisher.
+            SteamVR_Behaviour_Pose m_Pose = m_Interactable.m_ActiveHand.GetComponent<SteamVR_Behaviour_Pose>();
+            
+            // Button pressed down
+            if (m_ActivateAction.GetStateDown(m_Pose.inputSource))
+            {
+                // If down, emit particles
+            }
+
+            // Button not pressed
+            if (m_ActivateAction.GetStateUp(m_Pose.inputSource))
+            {
+                // If up, no particles emmitted
+            }
+        }
+	}
+}
