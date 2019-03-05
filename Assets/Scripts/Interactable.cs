17caa3addd43418908efb0876fd52f449b87501f
Bird <bradenbird@tamu.edu>
2019-03-05 10:26:23 -0600

    Can pickup and throw objects in VR

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Interactable : MonoBehaviour {
+
+    [HideInInspector]
+    public Hand m_ActiveHand = null;
+
+}
