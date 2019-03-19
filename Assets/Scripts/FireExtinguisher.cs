f60d55c7ebb9ca4ded30d4ad0d0e06a38ca45515
Bird <bradenbird@tamu.edu>
2019-03-19 09:28:09 -0500

    Finished adding basic fire extinguisher functionality

--- Assets/Scripts/FireExtinguisher.cs
+++ Assets/Scripts/FireExtinguisher.cs
@@ -6,6 +6,7 @@ using Valve.VR;
 public class FireExtinguisher : MonoBehaviour {
 
     public SteamVR_Action_Boolean m_ActivateAction = null;
+    private bool lastFrameOn = false;
 
     // Use this for initialization
     void Awake ()
@@ -16,6 +17,7 @@ public class FireExtinguisher : MonoBehaviour {
     void Update ()
     {
         Interactable m_Interactable = GetComponent<Interactable>();
+        ParticleSystem m_ParticleSystem = GetComponentInChildren<ParticleSystem>() as ParticleSystem;
 
         // Is the fire extinguisher being held?
         if (m_Interactable.m_ActiveHand)
@@ -23,18 +25,32 @@ public class FireExtinguisher : MonoBehaviour {
             // Object is being held, we can now determine if we should be
             // activating the fire extinguisher.
             SteamVR_Behaviour_Pose m_Pose = m_Interactable.m_ActiveHand.GetComponent<SteamVR_Behaviour_Pose>();
-            
+
             // Button pressed down
             if (m_ActivateAction.GetStateDown(m_Pose.inputSource))
             {
+                lastFrameOn = true;
                 // If down, emit particles
                 print("Button down");
+                if (m_ParticleSystem.isStopped)
+                {
+                    m_ParticleSystem.Play(true);
+                }
             }
 
             // Button not pressed
             if (m_ActivateAction.GetStateUp(m_Pose.inputSource))
             {
-                // If up, no particles emmitted
+                if (lastFrameOn)
+                {
+                    print("Button released");
+                    lastFrameOn = false;
+                    if (m_ParticleSystem.isPlaying)
+                    {
+                        m_ParticleSystem.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+                    }
+                }
+                // If up, no particles emitted
             }
         }
 	}
