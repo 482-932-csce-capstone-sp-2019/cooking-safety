c79cac8d940e4dcfeb3fd6beb285194d2c8ac2bc
ncharchenko <ncharchenko@tamu.edu>
2019-03-28 10:26:32 -0500

    Sink stuff! TEST PLS!

---
+++ Assets/Scripts/Sink.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Valve.VR;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Sink : Interactable
+    {
+        private bool isRunning = false;
+        public void Poke(Hand activating_hand)
+        {
+            ParticleSystem water = this.GetComponent<ParticleSystem>();
+                // Turn on/off the sink
+                if (isRunning)
+                {
+                    isRunning = false;
+                /* Turn off sink
+                 * 1. Turn off water effect. */
+                water.Stop();
+                }
+                else
+                {
+                    isRunning = true;
+                /* Turn on sink 
+                 * 1. Turn on water effect. */
+                water.Play();
+                }
+        }
+     
+    }
+}
