5ebef47e1c489cc1d55d32c17943ecb679298430
Bird <bradenbird@tamu.edu>
2019-04-04 10:38:38 -0500

    Added spatula specific pickup, can probably be used for other tools if necessary. Counter collider adjusted.

--- Assets/Scripts/Spatula.cs
+++ Assets/Scripts/Spatula.cs
@@ -18,13 +18,45 @@ public class Spatula : Interactable {
         }
 
         // Position
-        Transform handPosition = hand.gameObject.transform;
-        transform.position = handPosition.position + handPosition.TransformDirection(new Vector3(x, y, z));
+
+        /*
+        Transform handTransform = hand.gameObject.transform;
+        transform.position = handTransform.position + handTransform.TransformDirection(new Vector3(x, y, z));
+        Vector3 angles = new Vector3(handTransform.eulerAngles.x, handTransform.eulerAngles.y, handTransform.eulerAngles.z);
+        transform.eulerAngles = angles;
         print("New transform");
+        */
+
+        print("Old transform: " + transform.position.x + " " + transform.position.y + " " + transform.position.z);
+        print("Old local transform: " + transform.localPosition.x + " " + transform.localPosition.y + " " + transform.localPosition.z);
+
+
+        transform.localPosition = hand.transform.position - transform.Find("InteractionPoint").localPosition;
+        transform.localRotation = Quaternion.Inverse(transform.Find("InteractionPoint").localRotation);
+        print("New transform: " + transform.position.x + " " + transform.position.y + " " + transform.position.z);
+        print("New local transform: " + transform.localPosition.x + " " + transform.localPosition.y + " " + transform.localPosition.z);
+
+        var renderer = hand.GetComponentInChildren<SkinnedMeshRenderer>();
+        renderer.enabled = false;
         hand.Attach(this);
 
         // Set active hand
         m_ActiveHand = hand;
     }
 
+    public override void Drop(Hand hand)
+    {
+        hand.copyVelocity(this);
+
+        var renderer = hand.GetComponentInChildren<SkinnedMeshRenderer>();
+        renderer.enabled = true;
+        // Detach
+        hand.Detach();
+
+        // Clear
+        m_ActiveHand = null;
+    }
+
 }
+
+
