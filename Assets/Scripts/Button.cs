af06c3b772595c027246ef9540de62b183eac42a
Braden Bird <bradenbird@tamu.edu>
2019-04-08 18:11:15 -0500

    Created MainMenu scene for starting game flow

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour {
+	
+    private void OnTriggerEnter(Collider other)
+    {
+        // If Hand enters collider, activate button
+        var hand = other.gameObject.GetComponent<Hand>();
+        if (hand)
+        {
+            // Change color of button for visual feedback
+            Renderer renderer = GetComponent<Renderer>();
+            Material old_mat = renderer.material;
+            GetComponent<Renderer>().material = Resources.Load("ButtonPressed", typeof(Material)) as Material;
+            Activate();
+            renderer.material = old_mat;
+        }
+
+    }
+
+    protected virtual void Activate()
+    {
+        // Should overrite in a child class
+        print("Activate not overridden");
+    }
+}
---
+++ Assets/Scripts/StartButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartButton : Button {
+
+	protected override void Activate()
+    {
+        // Load new scene asynchronously
+
+        // When finished, migrate to new scene
+    }
+}
