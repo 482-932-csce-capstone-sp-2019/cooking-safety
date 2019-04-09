9fb8620c8da75039223afc98c0d5763e30b34a6d
adarshkoka <koneello@gmail.com>
2019-04-09 09:35:41 -0500

    Thermometer script

---
+++ Assets/Scripts/Thermometer.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thermometer : MonoBehaviour {
+
+	public float temperature = 72;
+	Renderer rend;
+	
+	void Start () {
+		
+		rend = GetComponent<Renderer>();
+		rend.enabled = true;
+		
+		StartCoroutine(Cool());
+	}
+	
+	void OnTriggerEnter(Collider col)
+	{
+		if(col.gameObject.tag == "Meat")
+		{
+			Cookable cook = col.gameObject.GetComponent<Cookable>();
+			
+			float otherTemp = cook.core_temp;
+			
+			for(int i=0;i<otherTemp;i+=5)
+			{
+				temperature += i;
+			}
+		}
+		StartCoroutine("Heat");
+	}
+	void OnTriggerExit(Collider col)
+	{
+		StopCoroutine("Heat");
+	}
+	
+	IEnumerator Heat()
+	{
+		while(true)
+		{
+			yield return new WaitForSeconds(5);
+			temperature += 1;
+		}
+	}
+	IEnumerator Cool()
+	{
+		while(true)
+		{
+			yield return new WaitForSeconds(5);
+			temperature -= 1;
+		}
+	}
+}
