c64712cef9dd7c2d52a33603dfd273729ebb57d2
adarshkoka <koneello@gmail.com>
2019-04-16 11:11:31 -0500

    Darken cookable and kitchen tile mosaic material

--- Assets/Scripts/Cookable.cs
+++ Assets/Scripts/Cookable.cs
@@ -6,18 +6,49 @@ public class Cookable : MonoBehaviour {
 
     // Start at room temperature, later change to freezer temperature?
     public float core_temp = 35.0f;
+	
+	
     public bool on_griddle = false;
-
+	
+	public Color startColor;
+	public Color endColor;
+	public bool repeatable = false;
+	
+	public float time = 0.0f;
+	
+	Material mat;
+	
 	// Use this for initialization
 	void Start () {
 		
+		//startTime = Time.time;
+		mat = this.GetComponent<MeshRenderer>().material;
+		startColor = mat.color;
+		endColor = mat.color * .5f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (on_griddle)
         {
-            core_temp += 0.05f;
-        }
+			core_temp += 0.00005f;
+			float t = (time)*(core_temp);
+			GetComponent<Renderer>().material.color = Color.Lerp(startColor,endColor,t);
+			
+			time += 0.00001f;
+		}
+			
+			
+			
+			// if you want cookable to turn lighter again
+			/*
+			if(repeatable)
+			{
+				float t = (Mathf.Sin(Time.time - startTime)*(core_temp/100.0f));
+				GetComponent<Renderer>().material.color = Color.Lerp(startColor,endColor,t);
+			}
+			*/
+		
 	}
+	
 }
