08b4110b7d2dfaf872288433f054cd1d72b8d235
Braden Bird <bradenbird@tamu.edu>
2019-04-19 22:34:48 -0500

    Added zone to reset objects if they fall out of the user's reach

---
+++ Assets/Scripts/ResetPosition.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetPosition : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        Interactable obj = other.gameObject.GetComponent<Interactable>();
+        if (obj)
+        {
+            obj.Reset();
+        }
+    }
+}
