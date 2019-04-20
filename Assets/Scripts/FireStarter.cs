f8bd7bd8ca134d8389eed25403412d00dbe0ad3f
Braden Bird <bradenbird@tamu.edu>
2019-04-20 01:01:55 -0500

    Added random fire initialization, currently set to 10 seconds

--- Assets/Scripts/FireStarter.cs
+++ Assets/Scripts/FireStarter.cs
@@ -5,17 +5,24 @@ using UnityEngine;
 public class FireStarter : MonoBehaviour {
 
 	private Fire fire;
+    private float start_time;
 	// Use this for initialization
 	void Awake () {
 		fire = GetComponentInChildren<Fire>();
 		fire.gameObject.SetActive(false);
-		//TODO create random time for fire to start
-		start_fire(); //TODO remove
+        // Create random time for fire to start
+        System.Random rand = new System.Random();
+        start_time = (float) (rand.NextDouble() * 10);
+        print("Fire strt time will be in " + start_time.ToString() + " seconds?");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		//TODO check if fire should start
+		// Check if fire should start
+        if(start_time < Time.timeSinceLevelLoad)
+        {
+            start_fire();
+        }
 	}
 	
 	private void start_fire()
