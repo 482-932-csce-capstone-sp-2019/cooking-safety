75ca0ff5a32895d88d0038c49561539c1fbad66b
Braden Bird <bradenbird@tamu.edu>
2019-04-23 00:32:16 -0500

    Refactoring Thermometer.cs

--- Assets/Scripts/Thermometer.cs
+++ Assets/Scripts/Thermometer.cs
@@ -5,21 +5,13 @@ using TMPro;
 
 public class Thermometer : MonoBehaviour {
 
-	public int temperature = 0;
 	public enum State {None, Beef, Chicken};
-    Cookable cook;
-	Renderer rend;
     TextMeshPro display;
 	public State last_touched;
 	
     void Start()
     {
         display = transform.parent.gameObject.GetComponentInChildren<TextMeshPro>();
-    }
-
-	void Update()
-    {
-        display.text = System.Convert.ToString(temperature);
     }
 	
 	State ConvertToState(Meat.Meats m){
@@ -35,59 +27,24 @@ public class Thermometer : MonoBehaviour {
 	
 	void OnTriggerEnter(Collider col)
 	{
-        cook = col.gameObject.GetComponent<Cookable>();
+        Cookable cook = col.gameObject.GetComponent<Cookable>();
 
         if (cook)
 		{
 			
-			/*
-
-			float otherTemp = cook.core_temp;
-			for(int i=0;i<otherTemp;i+=5)
-			{
-				temperature += i;
-			}
-            */
-            //StartCoroutine("Heat");
-			
 			// Cross contamination check
 			State s = ConvertToState(cook.gameObject.GetComponent<Meat>().my_meat);
-			if(last_touched != State.None && s != last_touched){
-				Results.Fail("Cross contamination from thermometer. Must wash between different meats. FAIL");
-			}
-			else
-				temperature = (int)cook.core_temp;
-        }
-	}
-	void OnTriggerExit(Collider col)
-	{
-        if (col.gameObject.GetComponent<Cookable>())
-        {
-            //StopCoroutine("Heat");
-			last_touched = ConvertToState(cook.gameObject.GetComponent<Meat>().my_meat);
-            cook = null;
-        }
-	}
-	
-	IEnumerator Heat()
-	{
-		while(true)
-		{
-            //yield return new WaitForSeconds(5);
-            //temperature += 1;
+            if (last_touched != State.None && s != last_touched)
+            {
+                Results.Fail("Cross contamination from thermometer. Must wash between different meats. FAIL");
+            }
+            else
+            {
+                last_touched = ConvertToState(cook.gameObject.GetComponent<Meat>().my_meat);
+                int temperature = (int)cook.core_temp;
+                display.text = System.Convert.ToString(temperature);
+            }
 
-            float otherTemp = cook.core_temp;
-            temperature = (int)otherTemp;
-        }
-	}
-	IEnumerator Cool()
-	{
-		while(true)
-		{
-            //yield return new WaitForSeconds(5);
-            //temperature -= 1;
-            float otherTemp = cook.core_temp;
-            temperature = (int)otherTemp;
         }
 	}
 }
